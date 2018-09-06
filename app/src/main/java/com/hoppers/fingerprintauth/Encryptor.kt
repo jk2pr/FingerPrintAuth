21055b1d1b2a9f177f19e76db662965ac28c8ae9
jk2pr <jk2praj@gmail.com>
2018-09-06 16:06:11 +0800

    implemented saving data to keystore

---
+++ app/src/main/java/com/hoppers/fingerprintauth/Encryptor.kt
@@ -0,0 +1,48 @@
+package com.hoppers.fingerprintauth
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.support.annotation.NonNull
+import java.io.IOException
+import java.security.*
+import javax.crypto.*
+
+internal class Encryptor {
+
+    var encryption: ByteArray? = null
+    var iv: ByteArray? = null
+
+    @Throws(UnrecoverableEntryException::class, NoSuchAlgorithmException::class, KeyStoreException::class, NoSuchProviderException::class, NoSuchPaddingException::class, InvalidKeyException::class, IOException::class, InvalidAlgorithmParameterException::class, SignatureException::class, BadPaddingException::class, IllegalBlockSizeException::class)
+    fun encryptText(alias: String, textToEncrypt: String): ByteArray {
+
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(alias))
+
+        iv = cipher.iv
+
+        encryption = cipher.doFinal(textToEncrypt.toByteArray(charset("UTF-8")))
+        return encryption!!
+    }
+
+    @NonNull
+    @Throws(NoSuchAlgorithmException::class, NoSuchProviderException::class, InvalidAlgorithmParameterException::class)
+    private fun getSecretKey(alias: String): SecretKey {
+
+        val keyGenerator = KeyGenerator
+                .getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE)
+
+        keyGenerator.init(KeyGenParameterSpec.Builder(alias,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .build())
+
+        return keyGenerator.generateKey()
+    }
+
+    companion object {
+
+        private val TRANSFORMATION = "AES/GCM/NoPadding"
+        private val ANDROID_KEY_STORE = "AndroidKeyStore"
+    }
+}
--- app/src/main/java/com/hoppers/fingerprintauth/MainActivity.kt
+++ app/src/main/java/com/hoppers/fingerprintauth/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.hoppers.fingerprintauth
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Base64
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.UnrecoverableEntryException
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View) {
+
+        when (v.id) {
+            R.id.decrypt ->
+                decryptText()
+
+
+        }
+    }
+
+    private val TAG = MainActivity::class.java.simpleName
+    private val enc by lazy { Encryptor() }
+    private val dec by lazy { Decryptor() }
+    private val SAMPLE_ALIAS = "MYALIAS"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        decrypt.setOnClickListener(this)
+        encryptText()
+
+    }
+
+
+    private fun encryptText() {
+        try {
+            val encryptedText = enc
+                    .encryptText(SAMPLE_ALIAS, "Jitendra")
+            tvEncryptedText.text = Base64.encodeToString(encryptedText, Base64.DEFAULT)
+
+
+        } catch (e: Exception) {
+            Log.e(TAG, "onClick() called with: " + e.message, e)
+
+        }
+    }
+
+
+    private fun decryptText() {
+        try {
+            tvEncryptedText.text = dec
+                    .decryptData(SAMPLE_ALIAS, enc.encryption!!, enc.iv!!)
+        } catch (e: UnrecoverableEntryException) {
+            Log.e(TAG, "decryptData() called with: " + e.message, e)
+
+        }
     }
 }
